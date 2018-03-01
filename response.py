362aff46be186907372654aa91bfdb161d10340a
Joe Breda <joebreda@1x-nat-vl931-172-30-148-119.wireless.umass.edu>
2018-02-28 23:27:49 -0500

    again

--- response.py
+++ response.py
@@ -4,22 +4,16 @@ from twilio.twiml.voice_response import VoiceResponse
 app = Flask(__name__)
 
 
-@app.route("/record", methods=['GET', 'POST'])
-def record():
-    """Returns TwiML which prompts the caller to record a message"""
+@app.route("/voice", methods=['GET', 'POST'])
+def voice():
+    """Respond to incoming phone calls with a 'Hello world' message"""
     # Start our TwiML response
-    response = VoiceResponse()
+    resp = VoiceResponse()
 
-    # Use <Say> to give the caller some instructions
-    response.say('Hello. Please leave a message after the beep.')
+    # Read a message aloud to the caller
+    resp.say("hello world!", voice='alice')
 
-    # Use <Record> to record the caller's message
-    response.record()
-
-    # End the call with <Hangup>
-    response.hangup()
-
-    return str(response)
+    return str(resp)
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
